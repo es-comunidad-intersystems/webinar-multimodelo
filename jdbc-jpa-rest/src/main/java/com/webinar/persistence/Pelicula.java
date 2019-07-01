8edf297b54f06c75448b1a1c1531a8c8a3434e36
Luis Angel <luis-angel.perez-ramos@readycomputing.com>
2019-07-01 13:33:14 +0200

    Actualizacion de ususario de DB en proyecto JPA

--- jdbc-jpa-rest/src/main/java/com/webinar/persistence/Pelicula.java
+++ jdbc-jpa-rest/src/main/java/com/webinar/persistence/Pelicula.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import javax.persistence.*;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
